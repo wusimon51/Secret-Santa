66fead9f4e54ad8b2cae6ac6227b4561b1cd7337
wusimon51 <wusimon51@gmail.com>
2021-04-25 21:48:37 -0400

    update database name

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DB_PASSWORD,
-    database: 'mydb'
+    database: 'db'
 });
 
 connection.connect((err) => {
