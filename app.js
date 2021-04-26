09bf274acb5d68db8a3748cfa0a8060faff7b82b
wusimon51 <wusimon51@gmail.com>
2021-04-25 22:00:02 -0400

    update database port

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DB_PASSWORD,
-    database: 'db'
+    database: 'db',
+    port: 25060
 });
 
 connection.connect((err) => {
